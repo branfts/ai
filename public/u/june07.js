7f829307ba3df8c6dd0b93a47e347eca65d5e3fd
Will <11353590+june07@users.noreply.github.com>
2024-07-19 13:07:16 -0700

    .

--- public/u/june07.js
+++ public/u/june07.js
@@ -5,10 +5,5 @@ const links = [
         favicon: 'https://june07.com/favicon.ico'
     },
 ]
-const rules = [{
-    priority: 0,
-    name: 'redirect',
-    href: 'https://june07.com',
-    timeout: 10
-}]
+const rules = []
 export { links, rules }
